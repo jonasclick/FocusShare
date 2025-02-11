470322743c0b0453354f569f56076538e3c65205
Jonas Vetsch <jonas@vetsch.com>
2025-02-11 15:42:54 +0100

    Initial Commit

---
+++ FocusShare/FocusShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusShareApp.swift
+//  FocusShare
+//
+//  Created by Jonas Vetsch on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
